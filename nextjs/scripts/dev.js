a09e1a9ed9f2d8c4baae8d15a854f632344911cf
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2022-09-30 22:51:48 -0300

    Tweak dev script

--- nextjs/scripts/dev.js
+++ nextjs/scripts/dev.js
@@ -2,6 +2,7 @@ const { execSync } = require("child_process");
 const { port, cleanEnv } = require("envalid");
 const { existsSync, copyFileSync } = require("fs");
 const { config: readEnvironmentVariablesFromDotEnv } = require("dotenv");
+const Chalk = require("chalk");
 
 const main = () => {
   const env = loadEnvironmentVariables();
@@ -18,6 +19,11 @@ const loadEnvironmentVariables = () => {
   const dotEnvFileExists = existsSync(".env");
 
   if (!dotEnvFileExists) {
+    console.log(
+      Chalk.yellow(
+        "We didn't find a .env, so we're creating one for you from .env-sample.\n"
+      )
+    );
     createDotEnvFromSample();
   }
 
